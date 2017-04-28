7521852c9a7029d2373c9f2f45cf2363644ceb85
Alessandro Arnodo <alessandro@arnodo.net>
2017-04-28 12:18:09 +0200

    feat: add sse

--- src/server.js
+++ src/server.js
@@ -1,25 +1,45 @@
 const micro = require('micro')
 const cors = require('micro-cors')({ allowMethods: ['POST'] })
 const url = require('url')
+const SseChannel = require('sse-channel')
+const sse = new SseChannel({
+  cors: {
+    origins: ['*']
+  },
+  jsonEncode: true
+})
 const { json, send, sendError } = micro
 
 const createServer = feed => {
   async function storeLog (rawBody) {
+    // TODO: filter out not needed data
+    const data = rawBody
     return new Promise((resolve, reject) =>
-      feed.append(rawBody, err => (err ? reject(err) : resolve()))
+      feed.append(rawBody, err => {
+        if (err) return reject(err)
+        sse.send({
+          event: 'owt',
+          data
+        })
+        resolve()
+      })
     )
   }
 
   async function postHandler (req, res) {
     const body = await json(req)
     await storeLog(body)
-    send(res, 204)
+    send(res, 204, null)
   }
 
   async function getHandler (req, res) {
     const { pathname } = url.parse(req.url)
     if (pathname === '/favicon.ico') {
-      send(res, 204)
+      send(res, 204, null)
+      return
+    }
+    if (pathname === '/_live') {
+      sse.addClient(req, res)
       return
     }
     return feed.discoveryKey.toString('hex')
